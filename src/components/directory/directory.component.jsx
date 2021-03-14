c2dae55091655048135091d584dd295b648cc9f2
Shreejeeb Kesh <shreejeebkesh@gmail.com>
2021-03-14 21:00:19 +0530

    added  homepage and different components for homepage

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { SECTIONS } from "./directory.data.js";
+import MenuItem from "../menu-item/menu-item.component.jsx";
+import "./directory.styles.scss";
+class Directory extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      sections: SECTIONS,
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id }) => (
+          <MenuItem key={id} title={title} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
