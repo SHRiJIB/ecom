4a35ce95b532a1d49d2a604340342683de1aeae8
Shreejeeb Kesh <shreejeebkesh@gmail.com>
2021-03-24 16:47:30 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import "./App.css";
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={Shop} />
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CollectionPreview from "../../components/preview/collection-preview.component";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 import { SHOP_DATA } from "./shop.data";
 
 class Shop extends React.Component {
