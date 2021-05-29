70e8d09da269ddc0970052e313eb30f73f396776
Shreejeeb <shreejeebkesh@gmail.com>
2021-05-29 20:02:40 +0530

    avatar

---
+++ src/components/avatar/Avatar.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Avatar = ({ imgUrl, name = "Name" }) => {
+  return (
+    <div className="avatar">
+      {imgUrl ? (
+        <img src={imgUrl} alt={name} />
+      ) : (
+        <span>{name[0].toUpperCase()}</span>
+      )}
+    </div>
+  );
+};
+
+export default Avatar;
