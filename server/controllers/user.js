29ea94d1ce799f94534243b112ce89a9d6f14317
Shreejeeb <shreejeebkesh@gmail.com>
2021-05-29 20:04:45 +0530

    server:basic set up and user auth

---
+++ server/controllers/user.js
@@ -0,0 +1,56 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.js");
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(400).json({ message: "User does not exists." });
+    }
+    const isCorrectPassword = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isCorrectPassword) {
+      return res.status(400).json({ message: "Password is not correct." });
+    }
+
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      "ecom23",
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ result: existingUser, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong." });
+  }
+};
+const signup = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "User with this email already exists." });
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({
+      name: `${firstName} ${lastName}`,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+    const token = jwt.sign({ email: user.email, id: user._id }, "ecom23", {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ result: user, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong." });
+  }
+};
+
+module.exports = { signin, signup };
---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRouter = require("./routes/user.js");
+const app = express();
+dotenv.config();
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/user", userRouter);
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`SERVER is running on port:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+mongoose.set("useFindAndModify", false);
