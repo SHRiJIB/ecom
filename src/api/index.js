5fe2bdfd26e61436c737bcd2197386ab4f6600c3
Shreejeeb <shreejeebkesh@gmail.com>
2021-09-06 14:21:49 +0530

    api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://ecom23.herokuapp.com" });
 
 export const signin = (formData) => API.post("/user/signin", formData);
 export const signup = (formData) => API.post("/user/signup", formData);
