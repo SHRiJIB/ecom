81c7e8aeb50b5c1ce49348710de31274e98336db
Shreejeeb <shreejeebkesh@gmail.com>
2022-01-08 12:26:22 +0530

    nested shop routes

---
+++ src/Screens/category/CategoryPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CategoryPage: React.FC = () => {
+	return <div className="category">CATEGORY PAGE</div>;
+};
+
+export default CategoryPage;
--- src/Screens/shop/shop.component.tsx
+++ src/Screens/shop/shop.component.tsx
@@ -2,12 +2,21 @@ import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { selectShopCollections } from 'Stores/shop/shop.selector';
 import CollectionOverview from 'Components/collections-overview/CollectionOverview';
+import { Route, useLocation } from 'react-router';
+import CategoryPage from 'Screens/category/CategoryPage';
 
 const Shop: FC = () => {
 	const collections = useSelector(selectShopCollections);
+	const location = useLocation();
+	// console.log(location.pathname);
 	return (
 		<div className="shop-page">
-			<CollectionOverview collections={collections} />
+			<Route
+				exact
+				path={location.pathname}
+				component={() => <CollectionOverview collections={collections} />}
+			/>
+			<Route exact path={`${location.pathname}/:category`} component={CategoryPage} />
 		</div>
 	);
 };
