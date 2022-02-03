3159b71f0740de45a619080c03cf81261add7e76
Shreejeeb <shreejeebkesh@gmail.com>
2022-02-03 22:49:31 +0530

    Update cartitem selector

--- src/Stores/cart/cart.selector.ts
+++ src/Stores/cart/cart.selector.ts
@@ -1,16 +1,11 @@
 import { createSelector } from 'reselect';
 import { RootState } from 'Stores';
-import { CartItems, CartState } from './cart.reducer';
+import { CartState } from './cart.reducer';
 
 export const selectCart = (state: RootState): CartState => state.cart;
-export const selectCartItems = createSelector([selectCart], (cart) => cart.cartItems);
-export const selectCartItemsCount = createSelector([selectCartItems], (items: CartItems) =>
-	Object.keys(items).reduce((count, key) => count + items[key].quantity, 0)
-);
+export const selectCartItems = createSelector([selectCart], ({ cartItems }) => Object.keys(cartItems).map((key) => cartItems[key]));
+export const selectCartItemsCount = createSelector([selectCartItems], (items) => items.reduce((count, item) => count + item.quantity, 0));
 export const selectCartDropDownStatus = createSelector([selectCart], (cart) => cart.isOpen);
-export const selectCartTotal = createSelector([selectCartItems], (items: CartItems) =>
-	Object.keys(items).reduce(
-		(totalPrice, key) => totalPrice + items[key].quantity * items[key].price,
-		0
-	)
+export const selectCartTotal = createSelector([selectCartItems], (items) =>
+	items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0)
 );
--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import { RootState } from 'Stores';
-import { CartItems } from 'Stores/cart/cart.reducer';
+import { ICartItem } from 'Stores/cart/cart.reducer';
 import { toggleCart } from 'Stores/cart/cart.actions';
 import { selectCartItems } from 'Stores/cart/cart.selector';
 import CustomButton from 'Components/custom-button/custom-button.component';
@@ -11,15 +11,14 @@ import './cart-dropdown.styles.scss';
 
 interface CartDropDownProps {}
 const CartDropDown: React.FC<CartDropDownProps> = () => {
-	const cartItems = useSelector<RootState, CartItems>(selectCartItems);
+	const cartItems = useSelector<RootState, ICartItem[]>(selectCartItems);
 	const dispatch = useDispatch();
-	const cartItemsIds = Object.keys(cartItems);
 	const history = useHistory();
 	return (
 		<div className="cart-dropdown">
 			<div className="cart-items">
-				{cartItemsIds.length ? (
-					cartItemsIds.map((id) => <CartItemComp {...cartItems[id]} key={id} />)
+				{cartItems.length ? (
+					cartItems.map((cartItem) => <CartItemComp {...cartItem} key={cartItem.id} />)
 				) : (
 					<span className="empty-message">Your Cart is Empty.</span>
 				)}
